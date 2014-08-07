5baac2de8ed5af82607d68fd9831f0c026943857
Zubair Ahmed <zubair1024@gmail.com>
2014-08-08 01:59:25 +0530

    0.1.0
    Google complete

--- google/app/classes/GoogleAuth.php
+++ google/app/classes/GoogleAuth.php
@@ -10,8 +10,43 @@ class GoogleAuth {
         $this->client = $google_client;
         $this->client->setClientId('1097993448916-7ducj7ijnm8acp627it8a2ijmbvemp61.apps.googleusercontent.com');
         $this->client->setClientSecret('8tk6ruYQT8NyJj5MOzz9GUhQ');
-        $this->client->setRedirectUri('http://localhost:8888/login/google/index.php');
+        $this->client->setRedirectUri('http://localhost/login/google/index.php');
         $this->client->setScopes('email');
+    }
+
+    public function checkToken() {
+
+        if (isset($_SESSION['access_token']) && !empty($_SESSION['access_token'])) {
+            $this->client->setAccessToken($_SESSION['access_token']);
+        } else {
+            return $this->client->createAuthUrl();
+        }
+        return '';
+    }
+
+    public function login() {
+        if (isset($_GET['code'])) {
+            $this->client->authenticate($_GET['code']);
+            $_SESSION['access_token'] = $this->client->getAccessToken();
+            $this->storeUser($this->getPayload());
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+    public function logout() {
+        unset($_SESSION['access_token']);
+    }
+
+    public function getPayload() {
+
+        return json_decode(json_encode($this->client->verifyIdToken()->getAttributes()))->payload;
+    }
+
+    public function storeUser($payload) {
+        $sql = " insert into google_users (id,email) values ({$payload->id},'{$payload->email}' on duplicate key update id=id";
+        $this->db->query($sql);
+        
     }
 
 }
--- google/index.php
+++ google/index.php
@@ -1,6 +1,28 @@
 <?php
-
 require_once 'app/init.php';
 $db = new DB();
 $google_client = new Google_Client();
 $auth = new GoogleAuth($db, $google_client);
+$authUrl = $auth->checkToken();
+if ($auth->login()) {
+    $redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+    header('Location : ' . filter_var($redirect, FILTER_SANITIZE_URL));
+}
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <title>Login</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <p>
+            <?php if ($authUrl): ?>
+                <a href="<?= $authUrl ?>">Sign in with Google</a>
+            <?php else: ?>
+                <a href="logout.php">Logout</a>
+            <?php endif; ?>
+        </p>
+    </body>
+</html>
